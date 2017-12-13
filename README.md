8c2c15676b9c35421a0504bd2b734ec772ef2cd4
Valentin <valentin_tsipan@mail.ru>
2017-12-13 02:52:07 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Helpers
+=========================
+
+## Installation
+
+```
+$ composer require linnella/helpers:dev-master
+
+$ php artisan admin:import helpers
+```
+License
+------------
+Licensed under [The MIT License (MIT)](LICENSE).
---
+++ src/HelpersServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Core\Admin\Helpers;
+
+use Illuminate\Support\ServiceProvider;
+
+class HelpersServiceProvider extends ServiceProvider
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'admin-helpers');
+
+        Helpers::boot();
+    }
+}
